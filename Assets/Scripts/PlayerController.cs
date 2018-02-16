e285bdef8458a8eb2b1e3a9fcc0fa3b6e9f3f9a7
Surue <nicolas06062000@hotmail.com>
2018-02-16 07:53:35 +0100

    Show text when players can interact with something

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,21 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    Text informationnalText;
+    [SerializeField]
+    int playerIndex;
+    [SerializeField]
+    float speed;
+
     float horizontal;
     float vertical;
     Vector3 movement;
     Rigidbody body;
-    [SerializeField]
-    float speed;
     SpringJoint spring;
     bool springed;
+
     public bool objectInHand = false;
 
-    [SerializeField]
-    int playerIndex;
+    bool textShownOnce = false;
+    bool textHideOnce = false;
 
 	void Start ()
     {
@@ -28,6 +35,15 @@ public class PlayerController : MonoBehaviour
         horizontal = ControllersManager.Instance.GetAxis("Horizontal", playerIndex);
         vertical = ControllersManager.Instance.GetAxis("Vertical", playerIndex);
         movement = new Vector3(-horizontal * speed, 0, -vertical * speed);
+
+        if(textShownOnce && !textHideOnce) {
+            informationnalText.text = "You can interact with this object";
+        }else if(textHideOnce && !textShownOnce) {
+            informationnalText.text = "";
+        }
+
+        textShownOnce = false;
+        textHideOnce = false;
     }
 
     void FixedUpdate()
@@ -38,6 +54,18 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Interactive") {
+            textShownOnce = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision) {
+        if(collision.gameObject.tag == "Interactive") {
+            textHideOnce = true;
+        }
     }
 
     public int GetPlayerIndex() {
